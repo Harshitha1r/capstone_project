b3989b3975589dc6ce2c2c6937a2a3843f8dc023
Harshitha1r <harshithar2009@gmail.com>
2024-02-07 03:40:50 -0500

    validation,route changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import { Navbar } from './Components/navbar/Navbar';
+import { LandingPage } from './Components/Landing_Page/Landing_Page';
+import {Sign_Up} from './Components/Sign_up/Signup';
+import {Login} from './Components/login/Login'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <BrowserRouter>
+        <Navbar/>
+        <Routes>
+            <Route path='/' Component={LandingPage}/>
+            <Route path='/signup' Component={Sign_Up}/>
+            <Route path='/login' Component={Login}/>
+        </Routes>
+        </BrowserRouter>
     </div>
   );
 }
