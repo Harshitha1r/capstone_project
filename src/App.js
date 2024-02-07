7524da615d8d28d1cd76b349a0bee72d347e5acd
Harshitha1r <harshithar2009@gmail.com>
2024-02-07 04:22:35 -0500

    notification changes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { LandingPage } from './Components/Landing_Page/Landing_Page';
 import {Sign_Up} from './Components/Sign_up/Signup';
 import {Login} from './Components/login/Login'
 import {InstantConsultation} from './Components/InstantConsultationBooking/InstantConsultation'
+import {Notification} from './Components/Notification/Notification'
 function App() {
   return (
     <div className="App">
@@ -16,7 +17,7 @@ function App() {
             <Route path='/signup' Component={Sign_Up}/>
             <Route path='/login' Component={Login}/>
             <Route path='/instant-consultation' Component={InstantConsultation}/>
-
+            <Route path='/Notification' Component={Notification}/>
         </Routes>
         </BrowserRouter>
     </div>
